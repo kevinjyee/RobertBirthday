073aa62de99825dd323cfc1acf67eb9e94f960f4
kevinjyee <kevjyee@gmail.com>
2017-01-14 10:57:45 -0600

    attempted to add music

--- src/js/openclose.js
+++ src/js/openclose.js
@@ -1,7 +1,16 @@
+ var music = document.createElement('iframe');
+music.setAttribute("id","music");
+music.setAttribute('src', 'https://www.youtube.com/embed/2ips2mM7Zqw?rel=0&amp;showinfo=0&amp;autoplay=1');
+document.body.appendChild(music);
+
 function open_card() {
     document.getElementById('front').className = 'open-card';
+  music.pause();
+  music.currentTime = 0;
+  document.body.removeChild(document.getElementById("music"));
 }
 
 function close_card() {
     document.getElementById('front').className = '';
+
 }
